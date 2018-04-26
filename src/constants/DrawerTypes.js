bd1dddaafd7b77a115b6dd0feeb043eb1d5a72a6
Tejas Rathod <happytejasrathod@gmail.com>
2018-04-26 14:34:30 +0530

    User Data Fetch Dummy API Integrated
    Done with Dummy Fetch of User Data

--- src/constants/DrawerTypes.js
+++ src/constants/DrawerTypes.js
@@ -5,3 +5,4 @@ export const GET_REACH_IN_TIME_REQUEST = 'get_reach_in_time_request';
 export const GET_REACH_IN_TIME_SUCCESS = 'get_reach_in_time_success';
 export const GET_REACH_IN_TIME_FAILURE = 'get_reach_in_time_failure';
 export const UPDATE_SHOW_CATEGORY = 'update_show_category';
+export const USER_DATA_FETCH_SUCCESS = 'user_data_fetch_success';
